5695f30ed43529f7b8b2cbec60d85ae4b23d3f3b
19Fabio84-hue <piancatellif@gmail.com>
2023-05-31 17:33:24 +0200

    .

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Main from './component/main'
+
+
+export default function App(){
+    
+   return <Main />
+}
+
---
+++ src/component/data.js
@@ -0,0 +1,14 @@
+export default [
+    {
+  id: 0,
+  emoji : "😀"
+},
+{
+    id:1 ,
+    emoji : "😍"
+},
+{
+    id: 2,
+    emoji : "😮‍💨"
+  },
+]
