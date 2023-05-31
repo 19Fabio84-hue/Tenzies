bc0abc4fc5b6716c2c9f5d9a536a9544c724eb13
19Fabio84-hue <piancatellif@gmail.com>
2023-05-31 17:42:17 +0200

    .

--- src/component/Die.js
+++ src/component/Die.js
@@ -5,7 +5,7 @@ export default function Die(props){
          backgroundColor :props.isHeld ? '#59E391' : ''}
     return(
       <>
-          {props.rolling && props.isHeld === false ? <h2 className='rolling-animation die'></h2>  :
+          {props.rolling && props.isHeld === false ? <h2 className='rolling-animation die'> </h2>  :
            <h2 className='die' style={styles} onClick={props.toggle}>
             {props.value}
            </h2>}
